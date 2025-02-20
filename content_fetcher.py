6d9fe542baf5906fc61ac649a857bf5cc15f6757
Ramji7070 <patelramji9177@gmail.com>
2025-02-20 16:44:22 +0530

    Update content_fetcher.py

--- content_fetcher.py
+++ content_fetcher.py
@@ -71,8 +71,9 @@ def filter_content(content, folder_path):
                     # Add multiple conditions to check the thumbnailurl
                     if 'drm/wv' in thumbnailurl:
                         new_url_parts = thumbnailurl.replace('drm/wv/', 'drm/').split('/')
-                        new_url = "/".join(new_url_parts[:5] + new_url_parts[6:])  # Remove the 6th part
-                        thumbnailurl = new_url
+                        if len(new_url_parts) > 6:
+                            new_url = "/".join(new_url_parts[:5] + new_url_parts[6:])  # Remove the 6th part
+                            thumbnailurl = new_url
                     elif 'cpvi' in thumbnailurl:
                         new_url = thumbnailurl.replace('cpvideocdn.testbook.com/streams/', 'cpvod.testbook.com/')
                         new_url = new_url.replace('/master.m3u8', '/playlist.m3u8')
@@ -81,15 +82,18 @@ def filter_content(content, folder_path):
                         new_url = thumbnailurl.replace('media-cdn.classplusapp.com/tencent', 'tencdn.classplusapp.com').replace('thumbnail.jpg', 'master.m3u8')
                         thumbnailurl = new_url
                     elif 'snapshots/' in thumbnailurl:
-                        new_url = thumbnailurl.replace('/snapshots', '')
-                        new_url = new_url.replace('videos.classplusapp.com/vod-9a3dfb', 'alisg-cdn-a.classplusapp.com')
-                        new_url = new_url.rsplit('/', 1)[0] + '/master.m3u8'
-                        thumbnailurl = new_url
+                        parts = thumbnailurl.split('/')
+                        if len(parts) > 5:
+                            # Adjust the URL as per the specified pattern
+                            new_url = f"https://media-cdn.classplusapp.com/alisg-cdn-a.classplusapp.com/{parts[5]}/master.m3u8"
+                            thumbnailurl = new_url
+
                     elif '4b06bf8d61c41f8310af9b2624459378203740932b456b07fcf817b737fbae27' in thumbnailurl:
                         new_url_parts = thumbnailurl.split('/')
-                        new_url = f'https://media-cdn-a.classplusapp.com/mig-01/b08bad9ff8d969639b2e43d5769342cc62b510c4345d2f7f153bec53be84fe35/{new_url_parts[-1]}/{new_url_parts[-1]}.m3u8'
-                        new_url = new_url.replace('.jpeg', '')
-                        thumbnailurl = new_url
+                        if len(new_url_parts) > 1:
+                            new_url = f'https://media-cdn-a.classplusapp.com/mig-01/b08bad9ff8d969639b2e43d5769342cc62b510c4345d2f7f153bec53be84fe35/{new_url_parts[-1]}/{new_url_parts[-1]}.m3u8'
+                            new_url = new_url.replace('.jpeg', '')
+                            thumbnailurl = new_url
                     elif 'pdf' in thumbnailurl:
                         thumbnailurl = thumbnailurl
                     elif '/1d' in thumbnailurl:
@@ -128,11 +132,12 @@ async def process_folder(session, folder_id, folder_path, file, indent=0):
         file.write(output)
 
     subfolders = await get_folders(session, folder_id)
-    tasks = [
-        process_folder(session, subfolder_id, f"{folder_path} / {subfolder_name}", file, indent + 2)
-        for subfolder_id, subfolder_name in subfolders.items()
-    ]
-    await asyncio.gather(*tasks)
+    if subfolders:
+        tasks = [
+            process_folder(session, subfolder_id, folder_path + " " + subfolder_name, file, indent + 2)
+            for subfolder_id, subfolder_name in subfolders.items()
+        ]
+        await asyncio.gather(*tasks)
 
 async def generate_content_file(msg):
     async with aiohttp.ClientSession() as session:
@@ -140,11 +145,12 @@ async def generate_content_file(msg):
 
         file_name = "batch_content.txt"
         with open(file_name, 'w', encoding='utf-8') as file:
-            tasks = [
-                process_folder(session, folder_id, folder_name, file)
-                for folder_id, folder_name in folders.items()
-            ]
-            await asyncio.gather(*tasks)
+            if folders:
+                tasks = [
+                    process_folder(session, folder_id, folder_name, file)
+                    for folder_id, folder_name in folders.items()
+                ]
+                await asyncio.gather(*tasks)
 
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         new_file_name = f"batch_content_{timestamp}.txt"
